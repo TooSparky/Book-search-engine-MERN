13fa375312c4dbf84f4e63b4e52b793be2369811
Zach Barnes <16zacharybarnes@gmail.com>
2023-11-09 09:57:51 -0600

    added new apolloServer code to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,34 @@
 import './App.css';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
 import { Outlet } from 'react-router-dom';
-
+import { setContext } from '@apollo/client/link/context';
 import Navbar from './components/Navbar';
+
+const httpLink = createHttpLink({
+  uri: 'graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink)
+})
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
